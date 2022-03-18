2ab0e96583873f7ff824896453f152ac607ce3a4
amalia <lucia.amaliaof@gmail.com>
2022-03-18 00:22:40 -0300

    Initial commit

---
+++ src/main/java/com/br/lucia/controlefinancas/ControleFinancasApplication.java
@@ -0,0 +1,13 @@
+package com.br.lucia.controlefinancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleFinancasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/lucia/controlefinancas/ControleFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.lucia.controlefinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
